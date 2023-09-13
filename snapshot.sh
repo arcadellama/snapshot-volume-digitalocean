138f4113d241b1f3b0e17025eee463987a78ecf0
arcadellama <arcadellama@posteo.net>
2023-09-13 13:27:33 -0500

    add options for daily, monthly, and weekly backups; also searches by tag name instead of snapshot name

--- snapshot.sh
+++ snapshot.sh
@@ -1,42 +1,73 @@
 #!/bin/bash
 
 api=""
+env_file=""
 now=$(date +"%Y-%m-%d")
 snapshot_name=""
-snapshot_tags=""
+snapshot_tag=""
 volume_name=""
 
+if [ -n "$env_file" ] && [ -r "$env_file" ]; then
+  . "$env_file" || exit 1
+fi
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --daily)
+      snapshot_tag="daily_backup"
+      shift
+      ;;
+    --weekly)
+      snapshot_tag="weekly_backup"
+      shift
+      ;;
+    --monthly)
+      snapshot_tag="monthly_backup"
+      shift
+      ;;
+    --env-file)
+      if [ -r "$2" ]; then
+        . "$2" || exit 1
+      fi
+      shift 2
+      ;;
+    *)
+      echo "Usage: snapshot.sh [--env-file <path>] [--daily|--weekly|--monthly]>"
+      exit 1
+      ;;
+  esac
+done
+
 ## check value of predefined variable
-if [ "$api" == "" ] || [ "$snapshot_name" == "" ] || [ "$snapshot_tags" == "" ] || [ "$volume_name" == "" ];
-then
-    echo "Please check your variable value"
-    exit 1
+if [ "$api" == "" ] || [ "$snapshot_name" == "" ] || [ "$snapshot_tag" == "" ] || [ "$volume_name" == "" ]; then
+  echo "Please check your variable value"
+  exit 1
 fi
 
 ## get volume id
 volume_id=$(curl -X GET \
-    -H 'Content-Type: application/json' \
-    -H 'Authorization: Bearer '$api \
-    "https://api.digitalocean.com/v2/volumes?name=$volume_name" | \
-jq -r '.volumes[].id')
+  -H 'Content-Type: application/json' \
+  -H 'Authorization: Bearer '$api \
+  "https://api.digitalocean.com/v2/volumes?name=$volume_name" \
+  | jq -r '.volumes[].id')
 
 # get snaphost id that would deleted
 snapshot_id=$(curl -X GET \
-    -H 'Content-Type: application/json' \
-    -H 'Authorization: Bearer '$api \
-    "https://api.digitalocean.com/v2/volumes/$volume_id/snapshots?page=1&per_page=1" | \
-jq '.snapshots | map(select((.created_at < "'$now'" and .name == "'$snapshot_name'")))' | \
-jq -r '.[].id')
+  -H 'Content-Type: application/json' \
+  -H 'Authorization: Bearer '$api \
+  "https://api.digitalocean.com/v2/volumes/$volume_id/snapshots?page=1&per_page=1" \
+  | jq '.snapshots | map(select((.created_at < "'$now'" ))) | map(select(.tag contains "'$snapshot_name'"))' \
+  | jq -r '.[].id')
 
 ## delete snapshot
 curl -X DELETE \
-    -H 'Content-Type: application/json' \
-    -H 'Authorization: Bearer '$api \
-    "https://api.digitalocean.com/v2/snapshots/$snapshot_id"
+  -H 'Content-Type: application/json' \
+  -H 'Authorization: Bearer '$api \
+  "https://api.digitalocean.com/v2/snapshots/$snapshot_id"
 
 ## create new snapshot today
 curl -X POST \
-    -H 'Content-Type: application/json' \
-    -H 'Authorization: Bearer '$api \
-    -d '{"name":"'$snapshot_name'","tags":["'$snapshot_tags'"]}' \
-    "https://api.digitalocean.com/v2/volumes/$volume_id/snapshots"
+  -H 'Content-Type: application/json' \
+  -H 'Authorization: Bearer '$api \
+  -d '{"name":"'$snapshot_name'","tags":["'$snapshot_tag'"]}' \
+  "https://api.digitalocean.com/v2/volumes/$volume_id/snapshots"
