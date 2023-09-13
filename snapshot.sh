6f9900d52444a23d1925bc9056a608d98100965b
arcadellama <arcadellama@posteo.net>
2023-09-13 15:46:06 -0500

    Added proper http/curl processing and error checking

--- snapshot.sh
+++ snapshot.sh
@@ -12,7 +12,6 @@ snapshot_name=""
 snapshot_tag=""
 volume_name=""
 
-
 inf() {
   echo "[$(date '+%Y-%m-%dT%TZ')] $*"
 }
@@ -21,17 +20,43 @@ err() {
   echo >&2 "[$(date '+%Y-%m-%dT%TZ')] ERROR: $*"
 }
 
+http__curl() (
+  # Usage: use as a stand-in for curl, less the -sS options.
+  retval=0 response="" ret=""
+  if ! response=$(mktemp 2>&1); then
+    printf %s\\n "$response"
+    return 1
+  fi
+
+  if ! ret=$("$(command -v curl)" -sS \
+    -o "$response" \
+    -w '%{http_code}' \
+    "$@" 2>&1); then
+    printf %s\\n "$ret" >"$response"
+    retval=1
+  fi
+
+  if [ $retval -eq 0 ] && [ "$ret" -ge 400 ]; then
+    printf 'HTTP Error Code: %s\n' "$ret"
+    retval=1
+  fi
+
+  cat "$response"
+  rm -rf "$response"
+  return $retval
+)
+
 main() {
   # Dependency check
   for x in curl jq; do
     if ! command -v "$x" >/dev/null 2>&1; then
       err "$x not installed or available in the PATH"
-      exit 1
+      return 1
     fi
   done
 
   if [ -n "$env_file" ] && [ -r "$env_file" ]; then
-    . "$env_file" || exit 1
+    . "$env_file" || return 1
   fi
 
   while [ $# -gt 0 ]; do
@@ -50,13 +75,13 @@ main() {
         ;;
       --env-file)
         if [ -r "$2" ]; then
-          . "$2" || exit 1
+          . "$2" || return 1
         fi
         shift 2
         ;;
       *)
         echo "Usage: snapshot.sh [--env-file <path>] [--daily|--weekly|--monthly]>"
-        exit 1
+        return 1
         ;;
     esac
   done
@@ -64,89 +89,69 @@ main() {
   ## check value of predefined variable
   if [ "$api" == "" ] || [ "$snapshot_name" == "" ] || [ "$snapshot_tag" == "" ] || [ "$volume_name" == "" ]; then
     err "Please check your variable value"
-    exit 1
+    return 1
   fi
 
   ## get volume id
-  if ! volume_id=$(curl -sS -X GET \
+  if ! volume_json=$(http__curl -X GET \
     -H 'Content-Type: application/json' \
     -H 'Authorization: Bearer '$api \
-    "https://api.digitalocean.com/v2/volumes?name=$volume_name" \
-    | jq -r '.volumes[].id' 2>&1); then
+    "https://api.digitalocean.com/v2/volumes?name=$volume_name" 2>&1); then
+    echo >&2 "$volume_json"
+    err "Unable to get volume id."
+    return 1
+  fi
+
+  if ! volume_id="$(echo "$volume_json" \
+    | jq -r '.volumes[].id' 2>&1)"; then
     echo >&2 "$volume_id"
     err "Unable to get volume id."
-    exit 1
+    return 1
   fi
 
   # get snaphost id that would deleted
-  if ! snapshot_id=$(curl -sS -X GET \
+  if ! snapshot_json=$(http__curl -X GET \
     -H 'Content-Type: application/json' \
     -H 'Authorization: Bearer '$api \
-    "https://api.digitalocean.com/v2/volumes/$volume_id/snapshots" \
+    "https://api.digitalocean.com/v2/volumes/$volume_id/snapshots" 2>&1); then
+    err "Unable to get snapshot id."
+    echo >&2 "$snapshot_json"
+    return 1
+  fi
+
+  if ! snapshot_id=$(echo "$snapshot_json" \
     | jq '.snapshots | map(select((.created_at < "'$now'" ))) | map(select(.tags[] | contains ("'$snapshot_tag'") ))' \
     | jq -r '.[].id' 2>&1); then
     err "Unable to get snapshot id."
     echo >&2 "$snapshot_id"
-    exit 1
+    return 1
   fi
 
   ## delete snapshot
   if [ -n "$snapshot_id" ]; then
-    if ! delete_id="$(curl -sS -X DELETE \
+    if ! delete_json="$(http__curl -X DELETE \
       -H 'Content-Type: application/json' \
       -H 'Authorization: Bearer '$api \
-      "https://api.digitalocean.com/v2/snapshots/$snapshot_id" \
-      | jq -r '.id' 2>&1)"; then
-      echo >&2 "$delete_id"
+      "https://api.digitalocean.com/v2/snapshots/$snapshot_id" 2>&1)"; then
+      echo >&2 "$delete_json"
       err "Unable to delete snapshot."
-      exit 1
+      return 1
     fi
-
-    # Deletion returns nothing, so an ID is a problem
-    if [ -n "$delete_id" ]; then
-      echo >&2 "$delete_id"
-      err "Unknown error deleting snapshot $snapshot_id."
-      exit 1
-    fi
-
     inf "Deleted snapshot id: $snapshot_id"
   fi
 
   ## create new snapshot today
-  if ! create_json="$(curl -sS -X POST \
+  if ! create_json="$(http__curl -X POST \
     -H 'Content-Type: application/json' \
     -H 'Authorization: Bearer '$api \
     -d '{"name":"'$snapshot_name'","tags":["'$snapshot_tag'"]}' \
-    "https://api.digitalocean.com/v2/volumes/$volume_id/snapshots")"; then
+    "https://api.digitalocean.com/v2/volumes/$volume_id/snapshots" 2>&1)"; then
     echo >&2 "$create_json"
     err "Unable to create new snapshot"
-    exit 1
-  fi
-  echo "DEBUG:"
-  echo "$create_json"
-
-  if ! create_id="$(echo "$create_json" | jq -r '.snapshots.id' 2>&1)"; then
-    echo >&2 "$create_id"
-    err "Unable to create new snapshot"
-    exit 1
+    return 1
   fi
 
-  # Check for error message
-  case "$create_id" in 
-    {\"message\"*) 
-      echo >&2 "$create_id"
-      err "Unable to create new snapshot."
-      exit 1
-      ;;
-  esac
-
-  # Error out if no creation id
-  if [ "$create_id" = "null" ] || [ -z "$create_id" ]; then
-    err "Unknown error creating new snapshot."
-    exit 1
-  fi
-
-  inf "Created new snapshot id $create_id with tag: $snapshot_tag"
+  inf "Created new snapshot with tag: $snapshot_tag"
 }
 
 main "$@"
